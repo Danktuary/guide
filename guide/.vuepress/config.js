dcfe886ab3b04c07dcfca73dbb5dc082913aaed4
Sanctuary <sanc.pw@gmail.com>
2018-09-26 17:53:27 -0400

    Add VuePress config file and temporary components

---
+++ guide/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: 'Discord.js Guide',
+	description: 'A guide made by the community of discord.js for its users.',
+	themeConfig: {
+		repo: 'discordjs/guide',
+		docsDir: 'guide',
+		sidebarDepth: 3,
+		editLinks: true,
+		lastUpdated: true,
+	},
+};
