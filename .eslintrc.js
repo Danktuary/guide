f7f8abae11b5d6aff02ffd87e43cd86bfdf85207
Sanctuary <sanc.pw@gmail.com>
2018-09-26 17:41:57 -0400

    Update the eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	extends: 'aqua/vue',
+	plugins: ['markdown'],
+	rules: {
+		'brace-style': ['error', 'stroustrup'],
+		'comma-dangle': ['error', 'always-multiline'],
+		'radix': ['error', 'as-needed'],
+		'no-negated-condition': 'off',
+		'operator-linebreak': ['error', 'before'],
+		'quote-props': ['error', 'as-needed'],
+		'vue/max-attributes-per-line': ['error', {
+			singleline: 4,
+			multiline: {
+				max: 1,
+				allowFirstline: false
+			}
+		}],
+		'vue/html-self-closing': ['error', {
+			html: {
+				void: 'always',
+				normal: 'never',
+				component: 'always',
+			},
+		}],
+		'vue/require-default-prop': 'off',
+		'vue/require-prop-types': 'off',
+	},
+};
